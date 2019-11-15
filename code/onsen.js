48dade628df45c014c84c034de74ff0c223367f8
JamieMctaggart <JamieMctaggart@computing.dundee.ac.uk>
2019-11-15 15:40:09 +0000

    basic three page onsen layout
    Co-Authored-By: null <robbieG123@users.noreply.github.com>
    Co-Authored-By: Matt Bain <matt-bain@users.noreply.github.com>

--- code/onsen.js
+++ code/onsen.js
@@ -1,11 +1,4 @@
-document.addEventListener('init', function(event) {
-  var page = event.target;
-
-  if (page.id === 'page1') {
-    page.querySelector('#push-button').onclick = function() {
-      document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: 'Page 2'}});
-    };
-  } else if (page.id === 'page2') {
-    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
-  }
+document.addEventListener('prechange', function(event) {
+  document.querySelector('ons-toolbar .center')
+    .innerHTML = event.tabItem.getAttribute('label');
 });
