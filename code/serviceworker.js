2e75d51f8ca0720fe020a979a11ab9dfbfc60290
MatthewBain <MatthewBain@computing.dundee.ac.uk>
2019-12-05 14:03:40 +0000

    add the install event listener
    Co-Authored-By: Matt Bain <matt-bain@users.noreply.github.com>

--- code/serviceworker.js
+++ code/serviceworker.js
@@ -8,3 +8,14 @@ var urlsToCache = [
   '/onsen.js',
   '/statistics.js'
 ];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
