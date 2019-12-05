eddd719612955240b7f1594a6814f9bf07fa3c4e
MatthewBain <MatthewBain@computing.dundee.ac.uk>
2019-12-05 14:03:16 +0000

    add the urls to be cached by the app
    Co-Authored-By: Matt Bain <matt-bain@users.noreply.github.com>

---
+++ code/serviceworker.js
@@ -0,0 +1,10 @@
+var CACHE_NAME = 'simple-PWA-localStorage';
+var urlsToCache = [
+  '/',
+  '../index.html',
+  '../results.html',
+  '../json/manifest.json',
+  '../css/master.css',
+  '/onsen.js',
+  '/statistics.js'
+];
