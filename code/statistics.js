5f3896fd73e7f51b7394b013d6349d38ed26a6d4
RobbieGardiner <RobbieGardiner@computing.dundee.ac.uk>
2019-12-05 13:52:55 +0000

    added info for fruit and veg entry

--- code/statistics.js
+++ code/statistics.js
@@ -23,6 +23,13 @@ function drawChart() {
 function showResult() {
 	drawChart();
 
+	if (checklist[0]) {
+		document.getElementById('checklist-item-'+(0+1)).innerHTML = "Shops such as the Little Green Larder on Perth Road, allow you to bring your own container for food you buy from the shop. Removing the need for plastic.";
+	}
+
+	if (checklist[1]) {
+		document.getElementById('checklist-item-'+(1+1)).innerHTML = "When shopping at your local supermarket you can buy your fruit and veg without plastic packaging which will greatly reduce wastefulness.";
+	}
 
 	if (checklist[2]) {
 		var toothbrushesPerYear = 6;
@@ -31,6 +38,8 @@ function showResult() {
 		document.getElementById('checklist-item-'+(2+1)).innerHTML = "If everyone in dundee used Bamboo toothbrushes, it would save aproximately "+dundeeToothbrushesPerYear+
 		" plastic toothbrushes per year, as plastic toothbrushes can't biodegrade, and require petrolium to be made";
 	}
+
+
 }
 
 var run = true;
